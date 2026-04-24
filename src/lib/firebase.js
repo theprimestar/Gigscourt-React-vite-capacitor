f81abd179838b49293b01a4415d56389c570a263
Gigscourt <theprimestarventures@gmail.com>
2026-04-24 23:23:48 +0100

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth, signInAnonymously } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBvbUwgjSJV7ebtpjZHQeyIH2tXIWnTIaA",
@@ -13,3 +14,10 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+export async function ensureFirebaseAuth() {
+  if (!auth.currentUser) {
+    await signInAnonymously(auth);
+  }
+}
