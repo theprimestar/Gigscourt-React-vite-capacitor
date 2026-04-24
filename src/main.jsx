129fcf17e289c031f18fe493d11b7e906e02cc51
Gigscourt <theprimestarventures@gmail.com>
2026-04-24 08:55:06 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css';
+import 'maplibre-gl/dist/maplibre-gl.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
