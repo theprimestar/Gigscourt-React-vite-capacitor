9ac8bb2f59105dbf06de2fecaf49ec376988e92c
Gigscourt <theprimestarventures@gmail.com>
2026-04-24 08:49:29 +0100

    Update Onboarding.jsx

--- src/screens/Onboarding.jsx
+++ src/screens/Onboarding.jsx
@@ -3,7 +3,6 @@ import { supabase } from '../lib/supabase';
 import { imagekitUrl, imagekitPublicKey } from '../lib/imagekit';
 import { IKContext, IKUpload } from 'imagekitio-react';
 import Map, { Marker } from 'react-map-gl';
-import 'maplibre-gl/dist/maplibre-gl.css';
 
 // Step 1: Name + Services
 function StepNameServices({ onNext }) {
