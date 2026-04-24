236d5851a37eba384dde0a87cc1e52aa29895058
Gigscourt <theprimestarventures@gmail.com>
2026-04-24 21:56:26 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'leaflet/dist/leaflet.css';
 import './App.css';
+import './App2.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
