88c582878337b7a8f5c059f6f3d74005a1da2afd
Gigscourt <theprimestarventures@gmail.com>
2026-04-24 01:29:56 +0100

    Create imagekit.js

---
+++ src/lib/imagekit.js
@@ -0,0 +1,6 @@
+import { IKContext } from 'imagekitio-react';
+
+const imagekitUrl = 'https://ik.imagekit.io/Theprimestar';
+const imagekitPublicKey = 'public_hwM9hldZI+DqFY/pncPQCA5VRWo=';
+
+export { imagekitUrl, imagekitPublicKey };
