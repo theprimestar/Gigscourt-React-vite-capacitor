abbb156518dfb0823eb1c9f3a9d9daf13c40ca5e
Gigscourt <theprimestarventures@gmail.com>
2026-04-24 15:07:01 +0100

    Update imagekit-auth.js

--- api/imagekit-auth.js
+++ api/imagekit-auth.js
@@ -1,4 +1,4 @@
-import ImageKit from 'imagekit';
+const ImageKit = require('imagekit');
 
 const imagekit = new ImageKit({
   publicKey: process.env.IMAGEKIT_PUBLIC_KEY || 'public_hwM9hldZI+DqFY/pncPQCA5VRWo=',
@@ -6,18 +6,11 @@ const imagekit = new ImageKit({
   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT || 'https://ik.imagekit.io/Theprimestar',
 });
 
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   try {
     const authParams = imagekit.getAuthenticationParameters();
-    res.status(200).json({
-      ...authParams,
-      _debug: {
-        publicKey: imagekit.options.publicKey ? 'present' : 'missing',
-        privateKey: imagekit.options.privateKey ? 'present' : 'missing',
-        urlEndpoint: imagekit.options.urlEndpoint,
-      }
-    });
+    res.status(200).json(authParams);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-}
+};
