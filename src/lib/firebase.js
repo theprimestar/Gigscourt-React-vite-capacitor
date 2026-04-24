5979afdf98ac76e23bb347bc23ae011b01916ae8
Gigscourt <theprimestarventures@gmail.com>
2026-04-24 21:48:19 +0100

    Create firebase.js

---
+++ src/lib/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvbUwgjSJV7ebtpjZHQeyIH2tXIWnTIaA",
+  authDomain: "the-gigscourt-project.firebaseapp.com",
+  projectId: "the-gigscourt-project",
+  storageBucket: "the-gigscourt-project.firebasestorage.app",
+  messagingSenderId: "590983642327",
+  appId: "1:590983642327:web:4e32feca3f2640b8158492",
+  measurementId: "G-VE1DT81N0T"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
