21b519aff5d1e2ee022f504bc3368dc15f4cae63
Gigscourt <theprimestarventures@gmail.com>
2026-04-25 12:14:48 +0100

    Update capacitor.config.js

--- capacitor.config.js
+++ capacitor.config.js
@@ -1,12 +1,14 @@
-{
-  "appId": "com.gigscourt.app",
-  "appName": "GigsCourt",
-  "webDir": "dist",
-  "server": {
-    "androidScheme": "https",
-    "iosScheme": "capacitor"
+const config = {
+  appId: 'com.gigscourt.app',
+  appName: 'GigsCourt',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https',
+    iosScheme: 'capacitor',
   },
-  "ios": {
-    "minVersion": "15.0"
-  }
-}
+  ios: {
+    minVersion: '15.0',
+  },
+};
+
+module.exports = config;
