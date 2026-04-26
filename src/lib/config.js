60152ce8feafc2b20c7151008ed68fc13345ba8b
Gigscourt <theprimestarventures@gmail.com>
2026-04-26 05:22:02 +0100

    Update config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,2 +1,1 @@
 export const IMAGEKIT_AUTH_URL = 'https://jyvkbhbkziyitfzehoxi.supabase.co/functions/v1/imagekit-auth';
-export const FIREBASE_MINT_TOKEN_URL = 'https://jyvkbhbkziyitfzehoxi.supabase.co/functions/v1/mint-firebase-token';
