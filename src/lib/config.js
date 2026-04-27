915830c0fc35b02f11cf35d0388fb4b8427540e1
Gigscourt <theprimestarventures@gmail.com>
2026-04-27 10:03:33 +0100

    Update config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,1 +1,2 @@
 export const IMAGEKIT_AUTH_URL = 'https://jyvkbhbkziyitfzehoxi.supabase.co/functions/v1/imagekit-auth';
+export const PUSH_NOTIFICATION_URL = 'https://jyvkbhbkziyitfzehoxi.supabase.co/functions/v1/send-push-notification';
