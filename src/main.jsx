e8c48fffa54990252221a5bb84bebd392f324c1c
Gigscourt <theprimestarventures@gmail.com>
2026-05-03 07:01:50 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'leaflet/dist/leaflet.css';
+import './global.css';
 import './App.css';
 import './App2.css';
 
